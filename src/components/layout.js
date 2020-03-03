715a9a1249b238ee7de6c2603d2a049bba9b6a27
gaeta <ja.gaeta@hotmail.com>
2020-03-02 22:38:12 -0300

    Example of Sass styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,9 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import "./bootstrap.min.css"
-import "./layout.css"
+// import "./bootstrap.min.css"
+// import "./layout.css"
+import "../sass/layout.scss"
 
 const Layout = ({ children }) => {
   return <>{children}</>
