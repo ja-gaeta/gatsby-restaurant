db3a464d926916a0628a30adead39f453ea71007
gaeta <ja.gaeta@hotmail.com>
2020-03-02 23:12:37 -0300

    Example of React Icons use

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import styled from "styled-components"
+import { FaBeer } from "react-icons/fa"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,13 +8,11 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h3>Hello from Gatsby!</h3>
-    <ButtonWrapper>Click Me</ButtonWrapper>
+    <h3>
+      {" "}
+      Lets go for a <FaBeer />?
+    </h3>
   </Layout>
 )
-
-const ButtonWrapper = styled.button`
-  background: blue;
-  color: white;
-`
 
 export default IndexPage
