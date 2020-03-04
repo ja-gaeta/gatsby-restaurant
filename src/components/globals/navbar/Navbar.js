8e00dd87586b2005856e4c4727c7505ee1be5992
gaeta <ja.gaeta@hotmail.com>
2020-03-03 21:03:56 -0300

    Implemented Navbar

--- src/components/globals/navbar/Navbar.js
+++ src/components/globals/navbar/Navbar.js
@@ -1,16 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 import NavbarHeader from "./NavbarHeader"
 import NavbarLinks from "./NavbarLinks"
 import NavbarIcons from "./NavbarIcons"
+import styled from "styled-components"
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false)
+  const handleNavbar = () => {
+    setNavbarOpen(!navbarOpen)
+  }
+
   return (
-    <nav>
-      <NavbarHeader />
-      <NavbarLinks />
+    <NavWrapper>
+      <NavbarHeader handleNavbar={handleNavbar} />
+      <NavbarLinks navbarOpen={navbarOpen} />
       <NavbarIcons />
-    </nav>
+    </NavWrapper>
   )
 }
+
+const NavWrapper = styled.nav`
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+  }
+`
 
 export default Navbar
