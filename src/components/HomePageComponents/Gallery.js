dfba2de7996c1dd68e87deb8007a1c44fc1f42c4
gaeta <ja.gaeta1@gmail.com>
2020-04-07 16:11:14 -0300

    Gallery section CSS Grid

--- src/components/HomePageComponents/Gallery.js
+++ src/components/HomePageComponents/Gallery.js
@@ -47,11 +47,11 @@ const Gallery = () => {
               </div>
               <div className="item item-2">
                 <Img fluid={img2} />
-                <p className="info">awesome pizza</p>
+                <p className="info">awesome pork</p>
               </div>
               <div className="item item-3">
                 <Img fluid={img3} />
-                <p className="info">awesome pizza</p>
+                <p className="info">awesome steak</p>
               </div>
             </GalleryWrapper>
           </Section>
@@ -61,6 +61,45 @@ const Gallery = () => {
   )
 }
 
-const GalleryWrapper = styled.div``
+const GalleryWrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto;
+  grid-row-gap: 1rem;
+  .item {
+    position: relative;
+  }
+  .info {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: ${styles.colors.mainYellow};
+    padding: 0.1rem 0.3rem;
+    text-transform: capitalize;
+  }
+  @media (min-width: 576px) {
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 1rem;
+  }
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (min-width: 992px) {
+    .gatsby-image-wrapper {
+      height: 100%;
+    }
+    grid-template-areas:
+      "one one two two"
+      "one one three three";
+    .item-1 {
+      grid-area: one;
+    }
+    .item-2 {
+      grid-area: two;
+    }
+    .item-3 {
+      grid-area: three;
+    }
+  }
+`
 
 export default Gallery
