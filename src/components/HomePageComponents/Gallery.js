1b66858b13376ae3fd413349a7e3ff55dacbda33
gaeta <ja.gaeta1@gmail.com>
2020-04-07 11:07:45 -0300

    Created Gallery component

---
+++ src/components/HomePageComponents/Gallery.js
@@ -0,0 +1,26 @@
+import React from "react"
+import styled from "styled-components"
+import { StaticQuery, graphql } from "gatsby"
+import { styles, Section } from "../../utils"
+import Img from "gatsby-image"
+
+const Gallery = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        {
+          img1: file(relativePath: { eq: "homeGallery/img-1.jpeg" }) {
+            childImageSharp {
+              fluid(maxWidth: 500) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      `}
+      render={data => <Img fluid={data.img1.childImageSharp.fluid} />}
+    />
+  )
+}
+
+export default Gallery
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,19 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { HomeHeader, Banner, BannerButton } from "../utils"
 import img from "../images/bcg/homeBcg.jpeg"
-import QuickInfo from '../components/HomePageComponents/QuickInfo'
+import QuickInfo from "../components/HomePageComponents/QuickInfo"
+import Gallery from "../components/HomePageComponents/Gallery"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HomeHeader img={img}>
       <Banner title="eatery" subtitle="55 main street - Santa Monica, CA">
-        <BannerButton style={{margin: '2rem auto'}}>menu</BannerButton>
+        <BannerButton style={{ margin: "2rem auto" }}>menu</BannerButton>
       </Banner>
     </HomeHeader>
     <QuickInfo />
+    <Gallery />
   </Layout>
 )
 
